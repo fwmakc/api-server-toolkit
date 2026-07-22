e457f297fd8e342f0e4a1f8e06babe0fdc3762f8
fwmakc <fwmakcpro@yandex.ru>
2026-07-23 00:10:54 +0300

    chore: remove dead files (PLAN.md, stale index.ts with EventBus exports)

--- index.ts
+++
@@ -1,7 +0,0 @@
-export { EventBusModule, EVENT_BUS_CLIENT } from "./event-bus.module";
-export { EventBusService } from "./event-bus.service";
-export {
-  EventBusPattern,
-  EventBusEnvelope,
-  EVENT_BUS,
-} from "./event-bus.constants";
