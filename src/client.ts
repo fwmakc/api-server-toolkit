1c804a3296b9ca2acebfe5ee0658680d7aa69b4c
fwmakc <fwmakcpro@yandex.ru>
2026-07-29 22:42:04 +0300

    feat: add subpath exports (/guard, /client) to avoid pulling passport for lightweight consumers

---
+++ src/client.ts
@@ -0,0 +1,3 @@
+export * from './common/client/event-client.interfaces';
+export * from './common/client/event-client.service';
+export * from './common/client/event-client.module';
---
+++ src/guard.ts
@@ -0,0 +1,4 @@
+export * from './common/guard/internal-auth.guard';
+export * from './common/guard/secure.guard.service';
+export * from './common/guard/secure.guard';
+export * from './common/guard/simple.secure.guard';
